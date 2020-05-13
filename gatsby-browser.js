6b7fcaa332a27f8f589a8bf1e6e48368afa31bfc
sunghaeJoung <shh03288@gmail.com>
2020-05-13 16:42:10 +0900

    클로저에 대해서, prisim js theme 변경

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 import "./src/styles/global.css"; // add from gatsby-browser for injecting style from all page
-import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/themes/prism.css";
 
 export { default as wrapRootElement } from "./src/theme";
